997e6b65f6ff437489f22cc8055adf76770df2ac
FelixWattsYork <hmq514@york.ac.uk>
2024-12-06 14:33:59 +0000

    sync

--- processing.py
+++ processing.py
@@ -15,7 +15,7 @@ def list_subsections(full_dataset):
 
 def list_indexes(full_dataset,subsection='zerod'):
     print("indexes in subsection " + subsection + ":")
-    print(full_dataset['post']['zerod'][0][0].dtype)
+    return full_dataset['post']['zerod'][0][0].dtype
 
 
 def get_variable(full_dataset,index, subsection='zerod'):
@@ -30,15 +30,15 @@ def get_average(full_dataset,start, end, index, subsection='zerod'):
 
 if __name__ == "__main__":
     full_dataset = scipy.io.loadmat("Complete/P1.5_B1_Idefault_Ndefault.mat")
-    list_subsections(full_dataset)
-    list_indexes(full_dataset)
-    print(get_average(full_dataset,50, 100, 'tite'))
-    tio = np.array(get_variable(full_dataset,'tite'))*np.array(get_variable(full_dataset,'te0'))
-    nio = np.array(get_variable(full_dataset,'ni0'))
-    taue = np.array(get_variable(full_dataset,"taue"))
-    tripple_product= tio*nio*taue
-    print(np.mean(tripple_product[50:100]))
-    plt.plot(tripple_product)
-    plt.show()
+    # list_subsections(full_dataset)
+    print(str(list_indexes(full_dataset)))
+    # print(get_average(full_dataset,50, 100, 'tite'))
+    # tio = np.array(get_variable(full_dataset,'tite'))*np.array(get_variable(full_dataset,'te0'))
+    # nio = np.array(get_variable(full_dataset,'ni0'))
+    # taue = np.array(get_variable(full_dataset,"taue"))
+    # tripple_product= tio*nio*taue
+    # print(np.mean(tripple_product[50:100]))
+    # plt.plot(tripple_product)
+    # plt.show()
 
 
